ccc0b895a4f9ee0f570253a5eb8c31938c1e4238
mfarukkoc <mfarukkoc@gmail.com>
2020-02-08 21:33:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Rock Paper Scissors Game with HTML, JS, CSS.
+From The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors)
---
+++ script.js
@@ -0,0 +1,46 @@
+var choices = ["Rock", "Paper", "Scissors"]
+
+var userScore = document.getElementById("usr-score")
+var cpuScore = document.getElementById("cpu-score")
+var round = document.getElementById("round-details")
+
+const winningMatrix = [
+    [0, -1, 1],
+    [1, 0, -1],
+    [-1, 1, 0]
+]
+
+const computerPlay = () => Math.floor((Math.random()*3))
+
+const playRound = (playerSelection) => {
+    computerSelection = computerPlay();
+    focusOnSelection(choices[playerSelection], choices[computerSelection]);
+    if(winningMatrix[playerSelection][computerSelection] == 1) {
+        round.innerHTML = ("You Win! " + choices[playerSelection] + " beats " + choices[computerSelection])
+        userScore.innerHTML = parseInt(userScore.innerHTML) + 1;
+    }
+    else if(winningMatrix[playerSelection][computerSelection] == -1) {
+        round.innerHTML =("You Lost! " + choices[computerSelection] + " beats " + choices[playerSelection])
+        cpuScore.innerHTML = parseInt(cpuScore.innerHTML) + 1;
+    }
+    else {
+        userScore.innerHTML = parseInt(userScore.innerHTML) + 1;
+        cpuScore.innerHTML = parseInt(cpuScore.innerHTML) + 1;
+        round.innerHTML = ("It's a Tie! " + choices[computerSelection] + " - " + choices[playerSelection])
+    }
+}
+
+const focusOnSelection = (user, cpu) => {
+    var selections = document.getElementsByClassName("selections")[0].children
+    for (var i = 0, len = selections.length; i < len; i++) {
+        selections[i].classList.remove("userSelected","cpuSelected","bothSelected")
+    }
+    if(user != cpu) {
+        document.getElementById(user).classList.add("userSelected")
+        document.getElementById(cpu).classList.add("cpuSelected")
+    }
+    else {
+        document.getElementById(user).classList.add("bothSelected")
+    }
+
+}
